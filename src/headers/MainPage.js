a45e01672ef5ba24fed4d5f23e670f457baf3669
Jessica Kim <jessikim807@gmail.com>
2020-10-27 14:28:40 -0400

    created header

---
+++ src/headers/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MainPage = (props) => {
+
+    return (
+        <div className="mainpage">
+            <div className="title">
+                <h1>Assignment Portal</h1>
+            </div>
+        </div>
+    );
+};
+
+export default MainPage;
